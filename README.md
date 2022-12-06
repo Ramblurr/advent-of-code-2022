2e3c0ae58d935b74d500b33caa690ef8398ba508
Casey Link <unnamedrambler@gmail.com>
2022-12-06 08:31:34 +0100

    Day 06

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 * [Day 03](./src/aoc/day03.clj)
 * [Day 04](./src/aoc/day04.clj)
 * [Day 05](./src/aoc/day05.clj)
+* [Day 06](./src/aoc/day06.clj)
 
---
+++ src/aoc/day06.clj
@@ -0,0 +1,40 @@
+(ns day06
+  (:require
+   [aoc.core :refer [read-input-lines]]))
+
+(def s1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+(def s2 "nppdvjthqldpwncqszvftbrmjlhg")
+(def s3 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+(def s4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+
+(defn base [n in]
+  (->> in
+       (partition n 1)
+       (take-while #(not (apply distinct? %)))
+       (count)
+       (+ n)))
+
+(base 4 s1)
+;; => 7
+(base 4 s2)
+;; => 6
+(base 4 s3)
+;; => 10
+(base 4 s4)
+;; => 11
+
+(defn part1 [fname]
+  (->> (read-input-lines fname)
+       first
+       (base 4)))
+
+(part1 "day06.txt")
+;; => 1300
+
+(defn part2 [fname]
+  (->> (read-input-lines fname)
+       first
+       (base 14)))
+
+(part2 "day06.txt")
+;; => 3986
